a4c552f4d90acdbd76f056add601408ed309da89
ZZAAAKK <48885763+ZZAAAKK@users.noreply.github.com>
2021-03-06 16:07:04 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import swift from './swift.png';
+import { BrowserView, MobileView, isBrowser, isMobile } from "react-device-detect";
+
+const buttons = [
+  "fart",
+  "eat",
+  "poop",
+  "cry",
+]
+
+const names = [
+  ["Swift","Death"],
+  ["Face","Masher"],
+  ["Beef","Goulash"]
+]
+
+const buttonObjects = buttons.map((button, i) => ({name:button, id:i}))
+const nameObjects = names.map((name, i) => ({forename:name[0], surname:name[1], id:i}))
+
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+function TestTable() {
+  return (
+    <div>
+      <style>{
+        "th{border:1px dotted black;}"
+      }</style>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => console.log("Hi")}>Forename</th>
+            <th>Surname</th>
+          </tr>
+        </thead>
+        <tbody>
+          {nameObjects.map((user, i) => {
+            return <tr key={user.id}><td>{user.forename}</td><td>{user.surname}</td></tr>
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 function App() {
+  const [action, setAction] = useState(buttons[Math.floor(Math.random() * buttons.length)] + "ing");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Swift is currently {action}.</h1>
+      <BrowserView>
+        <img src={swift} alt="The almighty Swift Death"/>
+        <section>
+          {buttonObjects.map((buttonObject) => {
+            return <button key={buttonObject.id} onClick={() => setAction(buttonObject.name + "ing")}>{toTitleCase(buttonObject.name)}</button>
+          })}
+        </section> 
+      </BrowserView>
+      <MobileView>
+        <img src={swift} width={window.innerWidth} alt="The almighty Swift Death"/>
+        <section>
+          {buttonObjects.map((buttonObject) => {
+            return <button key={buttonObject.id} onClick={() => setAction(buttonObject.name + "ing")}>{toTitleCase(buttonObject.name)}</button>
+          })}
+        </section>     
+      </MobileView>
+        <>
+          <TestTable />
+        </>
+    </>
   );
 }
 
